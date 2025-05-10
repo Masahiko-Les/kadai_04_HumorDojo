8c37cc4db539e200bf57a5e9c7dcc5dd360bbe66
Fujita-Masahiko <mfujita.jp@gmail.com>
2025-05-10 19:22:18 +0900

    修正

--- Script.js
+++ Script.js
@@ -1,3 +1,9 @@
 $('#first_page').on('click', function() { 
     $('#first_page_wrapper').fadeOut(2000);
 });
+
+$(".Theme_Box").on('click', function() {
+    $("this").find($("")).html("aaa");
+
+});
+
