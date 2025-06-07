56fa051f9817ee147ce4b1e5c1255232003fa901
FujitaMasa11 <mfujita.jp@gmail.com>
2025-06-07 20:46:46 +0900

    firebaseに設定する手前

--- Script.js
+++ Script.js
@@ -1,15 +1,19 @@
+// .Theme_Boxをクリックしたら.answer-formを表示する
 
-// .Theme_Boxをクリックしたら.answer-formを表示する（jQuery版）
-$(document).ready(function() {
-    $('.Theme_Box').on('click', function() {
-        $(this).find('.answer-form').show();
-    });
+$('.Theme_Box').on('click', function() {
+    $(this).find('.answer-form').show();
 });
 
-//.answer-formをクリックしたら、.Theme_Boxの.answer-formを非表示にする（jQuery版）
-// $(document).ready(function() {
-//     $('.answer-form').on('click', function(event) {
-//         event.stopPropagation(); // イベントのバブリングを防ぐ
-//         $(this).hide();
-//     });
-// };
+
+
+//#submitを押したら、your_answer1の内容を.answer_output_areaに出力する。
+$('#submit').on('click', function() {
+    let name= $('#your_name').val();
+    let answer = $('#your_answer1').val();
+    let html = '<div class="answer"><strong>' + name + ':</strong> ' + answer + '</div>'+ '<br>';
+    $('.answer_output_area').append(html);
+    
+    $('#your_name').val(''); // 名前入力欄をクリア
+    $('#your_answer1').val(''); // 入力欄をクリア
+    
+});
