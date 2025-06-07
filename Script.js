d2667311620f7641387d17da91ce6f4a8295c3af
FujitaMasa11 <mfujita.jp@gmail.com>
2025-06-07 20:01:25 +0900

    firebase用に構成を変更

--- Script.js
+++ Script.js
@@ -1,9 +1,15 @@
-$('#first_page').on('click', function() { 
-    $('#first_page_wrapper').fadeOut(2000);
+
+// .Theme_Boxをクリックしたら.answer-formを表示する（jQuery版）
+$(document).ready(function() {
+    $('.Theme_Box').on('click', function() {
+        $(this).find('.answer-form').show();
+    });
 });
 
-$(".Theme_Box").on('click', function() {
-    $("this").find($("")).html("aaa");
-
-});
-
+//.answer-formをクリックしたら、.Theme_Boxの.answer-formを非表示にする（jQuery版）
+// $(document).ready(function() {
+//     $('.answer-form').on('click', function(event) {
+//         event.stopPropagation(); // イベントのバブリングを防ぐ
+//         $(this).hide();
+//     });
+// };
