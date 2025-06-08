18eaa39e1c051906b3259619daedb572a268d290
FujitaMasa11 <mfujita.jp@gmail.com>
2025-06-08 15:35:59 +0900

    gitignoreを設定・configを別ファイルに設定

--- Script.js
+++ Script.js
@@ -6,14 +6,4 @@
 
 
 
-//#submitを押したら、your_answer1の内容を.answer_output_areaに出力する。
-$('#submit').on('click', function() {
-    let name= $('#your_name').val();
-    let answer = $('#your_answer1').val();
-    let html = '<div class="answer"><strong>' + name + ':</strong> ' + answer + '</div>'+ '<br>';
-    $('.answer_output_area').append(html);
-    
-    $('#your_name').val(''); // 名前入力欄をクリア
-    $('#your_answer1').val(''); // 入力欄をクリア
-    
-});
+
